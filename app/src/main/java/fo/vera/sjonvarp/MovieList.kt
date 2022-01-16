19fe7840623abf51540f84ea0215e52626c961a5
Pætur Magnussen <paetur.magnussen@gmail.com>
2022-01-16 19:54:49 +0000

    Init commit

---
+++ app/src/main/java/fo/vera/sjonvarp/MovieList.kt
@@ -0,0 +1,94 @@
+package fo.vera.sjonvarp
+
+object MovieList {
+    val MOVIE_CATEGORY = arrayOf(
+        "Sjónvarpsrásir",
+        "Sára og Dunna",
+        "My Little Pony",
+        "Vinus og Vimús",
+        "Grísa Peppa",
+        "Mouk"
+    )
+
+    val list: List<Movie> by lazy {
+        setupMovies()
+    }
+    private var count: Long = 0
+
+    private fun setupMovies(): List<Movie> {
+        val title = arrayOf(
+            "KVF",
+            "Google Demo Slam_ 20ft Search",
+            "Introducing Gmail Blue",
+            "Introducing Google Fiber to the Pole",
+            "Introducing Google Nose"
+        )
+
+        val description = "Fusce id nisi turpis. Praesent viverra bibendum semper. " +
+                "Donec tristique, orci sed semper lacinia, quam erat rhoncus massa, non congue tellus est " +
+                "quis tellus. Sed mollis orci venenatis quam scelerisque accumsan. Curabitur a massa sit " +
+                "amet mi accumsan mollis sed et magna. Vivamus sed aliquam risus. Nulla eget dolor in elit " +
+                "facilisis mattis. Ut aliquet luctus lacus. Phasellus nec commodo erat. Praesent tempus id " +
+                "lectus ac scelerisque. Maecenas pretium cursus lectus id volutpat."
+        val studio = arrayOf(
+            "KVF",
+            "Studio One",
+            "Studio Two",
+            "Studio Three",
+            "Studio Four"
+        )
+        val videoUrl = arrayOf(
+            "https://play.kringvarp.fo/redirect/kvf/_definst_/smil:kvf.smil?type=m3u8",
+            "https://play.kringvarp.fo/redirect/video/_definst_/smil:Sara_og_Dunna_01_OK.smil?type=m3u8",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue.mp4",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole.mp4",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose.mp4"
+        )
+        val bgImageUrl = arrayOf(
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review/bg.jpg",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search/bg.jpg",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue/bg.jpg",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole/bg.jpg",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/bg.jpg"
+        )
+        val cardImageUrl = arrayOf(
+            "https://kvf.fo/sites/all/themes/bootstrap_subtheme/favicon.ico",
+            "https://kvf.fo/sites/default/files/styles/news_xtra_xtra_large_2/public/sara_og_dunnan.jpg",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue/card.jpg",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole/card.jpg",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/card.jpg"
+        )
+
+        val list = title.indices.map {
+            buildMovieInfo(
+                title[it],
+                description,
+                studio[it],
+                videoUrl[it],
+                cardImageUrl[it],
+                bgImageUrl[it]
+            )
+        }
+
+        return list
+    }
+
+    private fun buildMovieInfo(
+        title: String,
+        description: String,
+        studio: String,
+        videoUrl: String,
+        cardImageUrl: String,
+        backgroundImageUrl: String
+    ): Movie {
+        val movie = Movie()
+        movie.id = count++
+        movie.title = title
+        movie.description = description
+        movie.studio = studio
+        movie.cardImageUrl = cardImageUrl
+        movie.backgroundImageUrl = backgroundImageUrl
+        movie.videoUrl = videoUrl
+        return movie
+    }
+}
